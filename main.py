5ad19d42bbd00ee5b162552e8d2edb1a7139b701
testName <test@gmail.com>
2026-01-27 17:59:49 +0530

    feat: Implement Postman API service to export collections and environments, and configure API access via environment variables.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Union
 import json
 import logging
 import sys
+import os
 from src.services.postman_service import PostmanService
 # Set up logging
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
@@ -16,17 +17,27 @@ def main_method():
         response = PostmanService().get_workspace(ws_id)   
         ws = response.json().get("workspace", {})
         collections = ws.get("collections", [])
+        environments = ws.get("environments", [])
         for collection in collections:
             col_id, col_name = collection["id"], collection["name"]
+            print(f"importing collection name : {col_name}")
             response = PostmanService().get_collection(col_id)
-            filename = f"{ws_name}-{col_name}.json"
             col_detail = response.json()
+            filename = os.path.join("output", ws_name, "collections", f"{col_name}.json")
+            os.makedirs(os.path.dirname(filename), exist_ok=True)
             with open(filename, "w") as f:
                 json.dump(col_detail, f, indent=2)
             print(f"Exported {col_name} from workspace {ws_name}")
-
-            print(f"importing collection name : {col_name}")
-
+        for environment in environments:
+            env_id, env_name = environment["id"], environment["name"]
+            print(f"importing environment name : {env_name}")
+            response = PostmanService().get_environment(env_id)
+            env_detail = response.json()
+            filename = os.path.join("output", ws_name, "environments", f"{env_name}.json")
+            os.makedirs(os.path.dirname(filename), exist_ok=True)
+            with open(filename, "w") as f:
+                json.dump(env_detail, f, indent=2)
+            print(f"Exported {env_name} from workspace {ws_name}")
 
 if __name__ == "__main__":
     main_method()
---
+++ src/services/postman_service.py
@@ -0,0 +1,53 @@
+import json
+import logging
+import base64
+import requests
+from requests.auth import HTTPBasicAuth
+from src.config import Config
+from src.utils.request_sender import RequestSender
+import sys
+
+# Set up logging
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+logger = logging.getLogger(__name__)
+
+class PostmanService:
+    def __init__(self):
+        self.request_sender:RequestSender = RequestSender()
+        self.postman_api_url = Config.postman_api_url
+        self.postman_api_key = Config.postman_api_key
+        self.headers = {
+            "Authorization": f"Bearer {self.postman_api_key}",
+            "Content-Type": "application/json"
+        }
+
+    def get_workspaces(self) -> requests.Response:
+        request_url = f"{self.postman_api_url}/workspaces"
+        response = self.request_sender.get_request(request_url, self.headers, None)
+        return response
+        
+    def get_workspace(self, id) -> requests.Response:
+        request_url = f"{self.postman_api_url}/workspaces/{id}"
+        response = self.request_sender.get_request(request_url, self.headers, None)
+        return response
+
+    def get_collections(self) -> requests.Response:
+        request_url = f"{self.postman_api_url}/collections"
+        response = self.request_sender.get_request(request_url, self.headers, None)
+        return response
+
+    def get_collection(self, id:str) -> requests.Response:
+        request_url = f"{self.postman_api_url}/collections/{id}"
+        response = self.request_sender.get_request(request_url, self.headers, None)
+        return response
+
+    def get_environments(self) -> requests.Response:
+        request_url = f"{self.postman_api_url}/environments"
+        response = self.request_sender.get_request(request_url, self.headers, None)
+        return response
+
+    def get_environment(self, id:str) -> requests.Response:
+        request_url = f"{self.postman_api_url}/environments/{id}"
+        response = self.request_sender.get_request(request_url, self.headers, None)
+        return response
+    
