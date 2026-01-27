3a739ddc3b7326c1f802f77e5eec9b2452a91eb0
testName <test@gmail.com>
2026-01-27 18:20:31 +0530

    export controls added

--- main.py
+++ main.py
@@ -3,17 +3,26 @@ import json
 import logging
 import sys
 import os
+from src.config import Config
 from src.services.postman_service import PostmanService
 # Set up logging
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 logger = logging.getLogger(__name__)
 
-
-def main_method():
+def export_workspace_list():
     response = PostmanService().get_workspaces()
     workspaces = response.json().get("workspaces", [])
+    print(f"Exporting workspace list")
+    json.dump(workspaces, open("output/workspaces.json", "w"), indent=2)
+    ws_list_json = {}
     for workspace in workspaces:
         ws_id, ws_name = workspace["id"], workspace["name"]
+        ws_list_json[ws_id] = ws_name
+    json.dump(ws_list_json, open("output/workspace_list.json", "w"), indent=2)
+    print(f"Exported workspace list")
+        
+def export_postman_data(workspaces:dict):
+    for ws_id, ws_name in workspaces.items():
         response = PostmanService().get_workspace(ws_id)   
         ws = response.json().get("workspace", {})
         collections = ws.get("collections", [])
@@ -38,6 +47,15 @@ def main_method():
             with open(filename, "w") as f:
                 json.dump(env_detail, f, indent=2)
             print(f"Exported {env_name} from workspace {ws_name}")
+        
+
+def main_method():
+    if Config.export_workspace_list.lower() == "true":
+        export_workspace_list()
+    if Config.export_postman_data.lower() == "true":
+        workspaces = json.load(open("output/workspace_list.json", "r"))
+        export_postman_data(workspaces)
+
 
 if __name__ == "__main__":
     main_method()
--- src/config.py
+++ src/config.py
@@ -8,3 +8,5 @@ load_dotenv()
 class Config:
     postman_api_url: str = os.environ["postman_api_url"]
     postman_api_key: str = os.environ["postman_api_key"]
+    export_workspace_list: str = os.environ["export_workspace_list"]
+    export_postman_data: str = os.environ["export_postman_data"]
